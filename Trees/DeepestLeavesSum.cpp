b4a133c982db8a4bd7ed66b1c640cb619850c8ff
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2021-06-20 02:32:15 +0530

    1302. Deepest Leaves Sum
    https://leetcode.com/problems/deepest-leaves-sum/

---
+++ Trees/DeepestLeavesSum.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        int sum = 0, prevSum = 0;
+        queue<TreeNode*> tn;
+        tn.push(root);
+        tn.push(NULL);
+        int level = 0;
+        while(tn.size()){
+            TreeNode* current = tn.front();
+            cout<<sum<<endl;
+            
+            if(tn.size() == 1 && current == NULL)
+                break;
+            
+            if(current == NULL && tn.size()>1){
+                tn.pop();
+                current = tn.front();
+                tn.push(NULL);
+                if(sum != 0){
+                    prevSum = sum;
+                    sum = 0;
+                }
+                
+            }           
+            
+            if(current->left){
+                tn.push(current->left);
+                sum+=current->left->val;
+            }
+                
+            if(current->right){
+                tn.push(current->right);
+                sum+=current->right->val;
+            }
+                
+            tn.pop();
+        }
+        return prevSum;
+    }
+};
