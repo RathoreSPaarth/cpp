921cd1a0a267a3be93ac9f4d78fd8a9fcc5f4542
Paarth S Rathore <47487547+RathoreSPaarth@users.noreply.github.com>
2021-06-28 23:54:17 +0530

    Create mergeSort.cpp
    Merge Sort

---
+++ sorting/mergeSort.cpp
@@ -0,0 +1,69 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void merge(int arr[], int left, int mid, int right){
+    int i = 0; 
+    int j = 0;
+    int k = left;
+    int n1 = mid-left+1;
+    int n2 = right - mid;
+    int L[n1];
+    int R[n2];
+    
+    for(int i = 0; i<n1; i++){
+        L[i] = arr[left+i];
+    }
+     for(int i = 0; i<n2; i++){
+        R[i] = arr[mid+1+i];
+    }
+    
+    while(i<n1 && j<n2){
+        if(L[i]<R[j]){
+            arr[k] = L[i];
+            i++;
+        }
+        else{
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while(i<n1){
+            	arr[k++] = L[i++];
+        	}
+    while(j<n2){
+               arr[k++] = R[j++];
+           } 
+        
+            
+}
+void mergeSort(int arr[],int left, int right){
+    if(left<right){
+        int mid = (left+right)/2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+        merge(arr,left, mid, right);
+    }
+}
+int main(){
+    
+    int t;
+    cin>>t;
+    while(t--){
+        int n;
+        cin>>n;
+        int arr[n];
+        
+        for(int i = 0; i<n; i++)
+            cin>>arr[i];
+        
+        mergeSort(arr, 0, n-1);
+        
+        for(int i = 0; i<n; i++){
+            cout<<arr[i]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
